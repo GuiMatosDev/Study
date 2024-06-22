d83a0309fcab582b96b0679bdfc273c609690c68
Guilherme M. <guilhermematos.ofc@gmail.com>
2024-06-22 09:00:33 -0300

    ajustes

--- 000_Projetos/media.c
+++ 000_Projetos/media.c
@@ -2,14 +2,41 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//Projeto para MÃ©dia de Vendedores
+//Projeto para Média de Vendedores
 
 int media(){
+	
+	char arquivo[200];
+	char nome[40];
+	long int vendas = 0;
+	long int vendas_semana_passada = 0;
+	int dia_util = 0;
+	int dias_vendidos = 0;
 	
 	setlocale(LC_ALL,"Portuguese");
 	
 	system("cls");
-	printf("MÃ©dia");
+	printf("================================\n            Media\n================================\n");
+	printf("Nome: ");
+	scanf("%s", &nome);
+	printf("Vendido atualmente: ");
+	scanf("%d", &vendas);
+	printf("Vendido semana passada: ");
+	scanf("%d", &vendas_semana_passada);
+	printf("Dias uteis desse mês: ");
+	scanf("%d", &dia_util);
+	printf("Dias vendidos: ");
+	scanf("%d", &dias_vendidos);
+	
+	
+	FILE *file;
+	file = fopen(arquivo,"w");
+	fprintf(file,"Nome:",nome);
+	
+	
+	fclose(file);
+	
+	
 }
 
 int main(){
@@ -35,7 +62,7 @@ int main(){
 			break;
 			
 			default:
-			printf("NÃ£o reconheci seu comando, vou te encaminhar novamente ao menu...");
+			printf("Não reconheci seu comando, vou te encaminhar novamente ao menu...");
 		}
 	}	
 }
